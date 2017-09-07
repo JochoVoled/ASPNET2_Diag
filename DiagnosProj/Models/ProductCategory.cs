1bc02660f65596c791be7a0169563fda0a16eb0a
JochoVoled <johannes@smidelov.com>
2017-09-07 09:56:27 +0200

    - Lägg till en ny model, ProductCategory, med en primärnyckel och egenskapen Name.

---
+++ DiagnosProj/Models/ProductCategory.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DiagnosProj.Models
+{
+    public class ProductCategory
+    {
+        [Key]
+        public int ProductCategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
